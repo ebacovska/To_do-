3640625e153d951da396e197b18d2dd60c9f6f1b
Elen Bačovská <elen.bacovska@gmail.com>
2023-01-03 11:35:39 +0100

    Add whole path

--- todolist_fanctions.py
+++ todolist_fanctions.py
@@ -1,8 +1,8 @@
-FILE_PATH = 'todo_text_file.txt'
+FILE_PATH = '/home/elen/Projekty/Poetry/todo_list/tests/Projekt_Github/To_do_list/todo_text_file.txt'
 
 def get_todos(filepath = FILE_PATH):
-    with open(filepath, 'r') as file:
-            todos_local = file.readlines() 
+    with open(filepath, 'r') as file_local:
+            todos_local = file_local.readlines() 
     return todos_local
 
 def write_todos(todos_arg, filepath = FILE_PATH):
